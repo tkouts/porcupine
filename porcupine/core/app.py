95bca18f143bad045b457bbd5a061e0971051f1d
Tassos Koutsovassilis <t.koutsovassilis@gmail.com>
2017-05-30 11:34:14 +0300

    Fixed synchronisation of db blueprints setup when using multiple workers

--- porcupine/core/app.py
+++ porcupine/core/app.py
@@ -1,5 +1,4 @@
 import sys
-
 import os
 import yaml
 from sanic import Blueprint
@@ -17,29 +16,16 @@ class App(Blueprint):
 
     def __init__(self):
         super().__init__(self.name)
-        self.listeners['before_server_start'].append(self.before_start)
-        self.listeners['after_server_start'].append(self.after_start)
-        self.listeners['before_server_stop'].append(self.before_stop)
-        self.listeners['after_server_stop'].append(self.after_stop)
 
-    async def before_start(self, app, loop):
+    async def setup_db_blueprint(self):
         if self.db_blueprint is not None:
             app_class_dir = os.path.abspath(
                 os.path.dirname(
-                    sys.modules[self.__class__.__module__].__file__))
+                    sys.modules[type(self).__module__].__file__))
             blueprint_file = os.path.join(app_class_dir, self.db_blueprint)
             with open(blueprint_file, encoding='utf-8') as f:
                 db_blueprint = yaml.load(f.read())
             await self.__initialize_db(db_blueprint)
-
-    def after_start(self, app, loop):
-        pass
-
-    def before_stop(self, app, loop):
-        pass
-
-    def after_stop(self, app, loop):
-        pass
 
     @with_context(SystemUser())
     @transactional()
--- porcupine/core/services/db.py
+++ porcupine/core/services/db.py
@@ -1,10 +1,15 @@
 """
 Database service
 """
+from multiprocessing import Lock
+
 from porcupine import log, db
 from porcupine.config import settings
 from porcupine.core import utils
+from porcupine.core.app import App
 from .service import AbstractService
+
+DB_BP_LOCK = Lock()
 
 
 class Db(AbstractService):
@@ -23,6 +28,16 @@ class Db(AbstractService):
         log.info('Opening database')
         db.connector = cls.get_connector()
         await db.connector.connect()
+        # allow only one process at a time
+        # to install the db blueprints
+        lock_acquired = DB_BP_LOCK.acquire(False)
+        if lock_acquired:
+            # install apps db blueprints
+            apps = [bp for bp in server.blueprints.values()
+                    if isinstance(bp, App) and bp.db_blueprint]
+            for app in apps:
+                await app.setup_db_blueprint()
+            DB_BP_LOCK.release()
 
     @classmethod
     async def stop(cls, server):
