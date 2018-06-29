260fe6ca9f76d1dc4a3661376c16297aa8fd4ce8
Tassos Koutsovassilis <t.koutsovassilis@gmail.com>
2018-06-29 15:33:05 +0300

    Export with_context

--- porcupine/__init__.py
+++ porcupine/__init__.py
@@ -3,7 +3,7 @@ from porcupine.core.log import porcupine_log as log
 
 # context
 from porcupine.core.server import server
-from porcupine.core.context import context, context_user
+from porcupine.core.context import context, context_user, with_context
 
 # utils
 from porcupine.core.utils import date, permissions, get_content_class, \
