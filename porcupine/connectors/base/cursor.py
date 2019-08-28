9646dacee311bfb6afb8f6d3fd5feffd109da250
Tassos Koutsovassilis <t.koutsovassilis@gmail.com>
2019-08-28 17:05:32 +0300

    Simplified cursors

--- porcupine/connectors/base/cursor.py
+++ porcupine/connectors/base/cursor.py
@@ -1,47 +1,13 @@
 import abc
 from typing import AsyncIterable
-from porcupine.core.stream.streamer import Streamer
+from namedlist import namedlist
+from porcupine.core.stream.streamer import IdStreamer
 
 
-class Range:
-    """
-    Range objects are used for setting cursor boundaries.
-    """
-    def __init__(self, lower_bound=None, upper_bound=None):
-        self.l_bound = lower_bound
-        self.l_inclusive = False
-        self.u_bound = upper_bound
-        self.u_inclusive = False
-        # self.set_lower_bound(lower_bound)
-        # self.set_upper_bound(upper_bound)
-
-    def set_lower_bound(self, lower_bound, inclusive=False):
-        self.l_bound = lower_bound
-        self.l_inclusive = inclusive
-
-    def set_upper_bound(self, upper_bound, inclusive=False):
-        self.u_bound = upper_bound
-        self.u_inclusive = inclusive
-
-    # def __contains__(self, value):
-    #     if self.l_bound is not None:
-    #         cmp_value = [-1]
-    #         if self.l_inclusive:
-    #             cmp_value.append(0)
-    #         cmp = (self.l_bound > value) - (self.l_bound < value)
-    #         if cmp not in cmp_value:
-    #             return False
-    #     if self.u_bound is not None:
-    #         cmp_value = [1]
-    #         if self.u_inclusive:
-    #             cmp_value.append(0)
-    #         cmp = (self.u_bound > value) - (self.u_bound < value)
-    #         if cmp not in cmp_value:
-    #             return False
-    #     return True
+Range = namedlist('Range', 'l_bound l_inclusive u_bound, u_inclusive')
 
 
-class BaseCursor(Streamer, metaclass=abc.ABCMeta):
+class BaseCursor(IdStreamer, metaclass=abc.ABCMeta):
     def __init__(self, index):
         self.index = index
         super().__init__(self.get_iterator())
@@ -55,9 +21,6 @@ class BaseCursor(Streamer, metaclass=abc.ABCMeta):
 
     def set(self, v):
         self.iterator.set(v)
-
-    def set_range(self, lower_bound, upper_bound):
-        self.iterator.set_range(lower_bound, upper_bound)
 
     def reverse(self):
         self.iterator.reverse()
@@ -87,9 +50,6 @@ class AbstractCursorIterator(AsyncIterable, metaclass=abc.ABCMeta):
 
     def set(self, v):
         self._bounds = v
-
-    def set_range(self, lower_bound, upper_bound):
-        self._bounds = Range(lower_bound, upper_bound)
 
     def reverse(self):
         self._reversed = not self._reversed
--- porcupine/connectors/couchbase/cursor.py
+++ porcupine/connectors/couchbase/cursor.py
@@ -1,6 +1,4 @@
-# from couchbase.exceptions import NotFoundError
 from couchbase.views.params import *
-
 from porcupine.connectors.base.cursor import BaseCursor, AbstractCursorIterator
 
 
@@ -50,22 +48,22 @@ class CursorIterator(AbstractCursorIterator):
             # range
             kwargs['mapkey_range'] = [
                 [self._scope, self._bounds.l_bound],
-                [self._scope, self._bounds.u_bound or
-                 Query.STRING_RANGE_END]
+                [self._scope, self._bounds.u_bound or Query.STRING_RANGE_END]
             ]
 
-
-        lower_bound = None
+        exclude_key = None
         if self._reversed:
             kwargs['mapkey_range'].reverse()
             kwargs['descending'] = True
             if is_ranged:
-                lower_bound = self._bounds.u_bound
+                if not self._bounds.u_inclusive:
+                    exclude_key = self._bounds.u_bound
                 if not self._bounds.l_inclusive:
                     kwargs['inclusive_end'] = False
         else:
             if is_ranged:
-                lower_bound = self._bounds.l_bound
+                if not self._bounds.l_inclusive:
+                    exclude_key = self._bounds.l_bound
                 if not self._bounds.u_inclusive:
                     kwargs['inclusive_end'] = False
 
@@ -77,37 +75,7 @@ class CursorIterator(AbstractCursorIterator):
             if self.reduce:
                 yield result.value
             else:
+                if exclude_key is not None and result.key[1] == exclude_key:
+                    continue
+                # TODO: return new uncommitted items
                 yield result.docid
-                # if self._range and not getattr(
-                #         self._range,
-                #         '_upper_inclusive' if self._reversed else '_lower_inclusive'):
-                #     if doc.key[1] == lower_bound:
-                #         continue
-                # if self.fetch_mode == 0:
-                #     if context._trans is not None and doc.docid in context._trans._locks:
-                #         if self._fetch_updated(doc.docid):
-                #             yield doc.docid
-                #         else:
-                #             continue
-                #     else:
-                #         yield doc.docid
-                # else:
-                #     if context._trans is not None and doc.docid in context._trans._locks:
-                #         updated = self._fetch_updated(doc.docid)
-                #         if updated:
-                #             yield updated
-                #         else:
-                #             continue
-                #     else:
-                #         item = self.resolve(doc.doc.value)
-                #         if item is not None:
-                #             yield item
-
-        # return new items last
-        # for k, v in new_items.items():
-        #     if self.fetch_mode == 0:
-        #         yield k
-        #     else:
-        #         item = self.resolve(v.value)
-        #         if item is not None:
-        #             yield item
