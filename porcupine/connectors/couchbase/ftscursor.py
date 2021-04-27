5ca62983ce98e276bbbe8d9d196d38353e256e3d
Tassos Koutsovassilis <t.koutsovassilis@gmail.com>
2021-04-27 10:53:55 +0300

    implemented fts cursor internal paging

--- porcupine/connectors/couchbase/ftscursor.py
+++ porcupine/connectors/couchbase/ftscursor.py
@@ -29,13 +29,24 @@ class FTSCursorIterator(FTSIndexIterator):
         scope_query = TermQuery(self.scope, field='pid')
         match_query = QueryStringQuery(self._term)
         query = ConjunctionQuery(match_query, scope_query)
-        results = cluster.search_query(
-            self.index.container_name,
-            query,
-            SearchOptions(
-                sort=['-_score'] if self._reversed else ['_score']
-            )
+        chunk_size = 20
+        skip = 0
+        options = SearchOptions(
+            sort=['-_score'] if self._reversed else ['_score'],
+            limit=chunk_size
         )
-        async for hit in results:
-            context.item_meta[hit.id] = hit.score
-            yield hit.id
+        returned = chunk_size
+        while returned == chunk_size:
+            results = cluster.search_query(
+                self.index.container_name,
+                query,
+                options,
+                skip=skip
+            )
+            returned = 0
+            async for hit in results:
+                returned += 1
+                print(hit.id)
+                context.item_meta[hit.id] = hit.score
+                yield hit.id
+            skip += chunk_size
