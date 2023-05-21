f4241b79d9fbd4ccce184487c2ce37fce7192dfe
Tassos Koutsovassilis <t.koutsovassilis@gmail.com>
2023-05-21 14:33:42 +0300

    new count and locate operators

--- porcupine/core/stream/operators.py
+++ porcupine/core/stream/operators.py
@@ -1,5 +1,5 @@
 from typing import Callable
-from aiostream import operator, streamcontext
+from aiostream import operator
 from sortedcontainers import SortedKeyList, SortedList
 
 
@@ -7,7 +7,7 @@ from sortedcontainers import SortedKeyList, SortedList
 async def sort(source, _reverse: bool = False):
     chunk = []
     sorted_list = SortedList()
-    async with streamcontext(source) as streamer:
+    async with source.stream() as streamer:
         async for i in streamer:
             chunk.append(i)
             if len(chunk) >= 20000:
@@ -24,7 +24,7 @@ async def sort(source, _reverse: bool = False):
 async def key_sort(source, key: Callable, _reverse: bool = False):
     chunk = []
     sorted_list = SortedKeyList(key=key)
-    async with streamcontext(source) as streamer:
+    async with source.stream() as streamer:
         async for i in streamer:
             chunk.append(i)
             if len(chunk) >= 20000:
@@ -36,10 +36,25 @@ async def key_sort(source, key: Callable, _reverse: bool = False):
         for i in sorted_list:
             yield i
 
-
 @operator(pipable=True)
 async def reverse(source):
-    async with streamcontext(source) as streamer:
+    async with source.stream() as streamer:
         result = [x async for x in streamer]
         for x in reversed(result):
             yield x
+
+@operator(pipable=True)
+async def count(source):
+    counter = 0
+    async with source.stream() as streamer:
+        async for _ in streamer:
+            counter +=1
+    yield counter
+
+
+@operator(pipable=True)
+async def locate(source, item):
+    async with source.stream() as streamer:
+        async for x in streamer:
+            if x == item:
+                yield x
