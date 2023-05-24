084c59fba8ef877b02e3daa42b76a7bf53dfe076
Tassos Koutsovassilis <t.koutsovassilis@gmail.com>
2023-05-24 12:25:00 +0300

    fixed offline context

--- porcupine/management/offlinecontext.py
+++ porcupine/management/offlinecontext.py
@@ -3,7 +3,7 @@ import asyncio
 import typing
 
 from porcupine.core.server import server
-from porcupine.core.services.db import Db
+from porcupine.core import services
 from porcupine.core.loader import import_all
 from porcupine.core.context import with_context
 from porcupine.apps.schema.users import SystemUser
@@ -11,11 +11,16 @@ from porcupine.apps.schema.users import SystemUser
 
 def execute(task: typing.Callable):
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(_exec(task))
+    loop.run_until_complete(_exec(task, loop))
 
 
-async def _exec(task: typing.Callable):
+async def _exec(task: typing.Callable, loop):
+    services.prepare_services(server)
     import_all(os.getcwd())
-    await Db.start(server)
+    db, schema = services.get_service('db'), services.get_service('schema')
+    await db.start(loop)
+    schema.start(loop)
     context_task = with_context(SystemUser())(task)
     await context_task()
+    await schema.stop(loop)
+    await db.stop(loop)
