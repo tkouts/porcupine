b84245f254b345ecb7395bff27a95327d9de51de
Tassos Koutsovassilis <t.koutsovassilis@gmail.com>
2023-05-24 23:54:35 +0300

    added concat and concatmap to available pipes

--- porcupine/pipe.py
+++ porcupine/pipe.py
@@ -15,17 +15,21 @@ getitem = pipe.getitem
 reduce = pipe.reduce
 flatten = pipe.flatten
 takelast = pipe.takelast
+concat = pipe.concat
+concatmap = pipe.concatmap
 print = pipe.print
-
-
-id_getter = map(lambda i: i.id)
-if_not_none = filter(lambda i: i is not None)
 
 key_sort = operators.key_sort.pipe
 reverse = operators.reverse.pipe
 sort = operators.sort.pipe
 count = operators.count.pipe
 locate = operators.locate.pipe
+
+def id_getter():
+    return map(lambda i: i.id)
+
+def if_not_none():
+    return filter(lambda i: i is not None)
 
 
 def skip_and_take(skp=0, tk=None):
